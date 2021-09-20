dafcd5dd8f8fc82ce49adb6b9bc3e0414d4aac99
gaetano <gaetano.riello@gmail.com>
2021-09-20 15:39:32 +0200

    update

--- script/backup.sh
+++ script/backup.sh
@@ -1,7 +1,7 @@
 # Svuota tabella tools
-mariadb -u root -ppassw0rd cfcm -e "TRUNCATE TABLE tools";
+mariadb -u admin -ppassw0rd cfcm -e "TRUNCATE TABLE tools";
 sleep 1
 # Backup del database
-mysqldump -u root -ppassw0rd cfcm --single-transaction --quick --lock-tables=false > /var/www/html/cfcm/cfcm.sql
+mysqldump -u admin -ppassw0rd cfcm --single-transaction --quick --lock-tables=false > /var/www/html/cfcm/cfcm.sql
 sleep 1
-cp -r /var/www/html/cfcm /home/cfcm/Backups/cfcm-"$(date +"%d-%m-%Y")"
+sudo tar -cvf /home/cfcm/Backups/cfcm-"$(date +"%d-%m-%Y-%h-%m-%s")".tar /var/www/html/cfcm/
