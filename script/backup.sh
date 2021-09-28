1f027f3e2c2f2ebb8659836b513580472d0a14d2
gaetano <gaetano.riello@gmail.com>
2021-09-28 16:30:52 +0200

    version: 1.0.2

--- script/backup.sh
+++ script/backup.sh
@@ -5,7 +5,7 @@ sleep 1
 mysqldump -u admin -ppassw0rd cfcm --single-transaction --quick --lock-tables=false > /var/www/html/cfcm/cfcm.sql
 sleep 1
 # Crea pacchetto tar
-tar -cvf /home/cfcm/Backups/cfcm-"$(date +"%d-%m-%Y-%h-%m-%s")".tar /var/www/html/cfcm/
+tar -cvf /home/cfcm/Backups/cfcm-"$(date +"%d-%m-%Y-%H-%M-%s")".tar /var/www/html/cfcm/
 sleep 1
 # Elimina Backup vecchi di 30 giorni
 find /home/cfcm/Backups/cfcm* -mtime +30 -exec rm -f {} \;
